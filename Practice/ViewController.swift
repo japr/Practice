b9027988fda3a6bdb0ff869f609a7ca5ceed89d9
Jorge Palacio <jorge.palacio@kisi.io>
2019-10-20 10:35:33 -0500

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Jorge Palacio on 10/20/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
